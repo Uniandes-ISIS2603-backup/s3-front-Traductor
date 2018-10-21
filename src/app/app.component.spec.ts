618132912489bba0d0aa146d560e568a2ff3eca7
Santiago Salazar <s.salazara@uniandes.edu.co>
2018-10-21 16:05:55 -0500

    Listos los tests de los comonentes listar hechos. LOS QUE SE HAGAN DE AQUI EN ADELANTE DEBEN AJUSTAR LOS TEST (.spec.ts) DE SUS LIST COMO ESTÁ EL DE EDITORIAL LIST PORFA. Añadido PhantomJS para correr los test y ya funciona.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { ClienteListComponent } from './cliente/cliente-list/cliente-list.component';
+import { PropuestaListComponent } from './propuesta/propuesta-list/propuesta-list.component';
+import { PagosListComponent } from './pagos/pagos-list/pagos-list.component';
+import { TarjetasListComponent } from './tarjeta-de-credito/tarjetas-list/tarjetas-list.component';
 
 describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ AppRoutingModule, HttpClientModule ],
+      providers: [{provide: APP_BASE_HREF, useValue: ''} ],
       declarations: [
+        ClienteListComponent,
+        PropuestaListComponent,
+        PagosListComponent,
+        TarjetasListComponent,
         AppComponent
       ]
     }).compileComponents();
@@ -28,5 +42,15 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to s3-Prometeus-Traductor!');
+  }));
+
+  it('should render titles in the navbar', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#clientesTag').textContent).toContain('Clientes');
+    expect(compiled.querySelector('#propuestasTag').textContent).toContain('Propuestas');
+    expect(compiled.querySelector('#tarjetasTag').textContent).toContain('Tarjetas de Credito');
+    expect(compiled.querySelector('#pagosTag').textContent).toContain('Pagos');
   }));
 });
--- src/app/propuesta/propuesta-list/propuesta-list.component.spec.ts
+++ src/app/propuesta/propuesta-list/propuesta-list.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppRoutingModule } from '../../app-routing/app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppModule } from '../../app.module';
 
 import { PropuestaListComponent } from './propuesta-list.component';
+import { PropuestaService } from '../propuesta.service';
+import { Propuesta } from '../propuesta';
 
 describe('PropuestaListComponent', () => {
   let component: PropuestaListComponent;
   let fixture: ComponentFixture<PropuestaListComponent>;
+  const propuestas: Propuesta[] = require('../../../assets/propuestas.json');
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PropuestaListComponent ]
+      imports: [ AppRoutingModule, HttpClientModule, AppModule ],
+      declarations: [ ],
+      providers: [{provide: APP_BASE_HREF, useValue: ''}, PropuestaService ]
     })
     .compileComponents();
   }));
@@ -21,5 +31,17 @@ describe('PropuestaListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('should have a list of propuestas', () => {
+    component.propuestas = propuestas;
+    expect(component.propuestas.length).toEqual(propuestas.length);
+  });
+
+  it('a propuesta should be a propuesta (first and last)', () => {
+    component.propuestas = propuestas;
+    //revisar todos los pagos
+    expect(component.propuestas[0].propuestaID).toEqual(propuestas[0].propuestaID);
+    expect(component.propuestas[propuestas.length - 1].propuestaID).toEqual(propuestas[propuestas.length - 1].propuestaID);
   });
 });
