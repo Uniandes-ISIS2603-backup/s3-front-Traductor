54768284d6cb421df88b3f2b7d15ccc2e3a2880e
Andres <a.manrique@uniandes.edu.co>
2018-11-05 21:19:04 -0500

    create tarjeta

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -6,15 +6,20 @@ import { ClienteDetailComponent } from './cliente-detail/cliente-detail.componen
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ClienteAddTarjetaDeCreditoComponent } from './cliente-add-tarjeta-de-credito/cliente-add-tarjeta-de-credito.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    CommonModule
+    CommonModule,
+    FormsModule,
+    NgbModule
   ],
-  declarations: [ClienteListComponent, ClienteDetailComponent],
+  declarations: [ClienteListComponent, ClienteDetailComponent, ClienteAddTarjetaDeCreditoComponent],
   providers: [ClienteService],
   exports: [ClienteListComponent, ClienteDetailComponent]
 })
