cbd46cef19af8be148bb327e2fda056cc0c2580a
Santiago Salazar <s.salazara@uniandes.edu.co>
2018-11-24 23:12:30 -0500

    creados los componentes para el login y para el registro.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [LoginComponent, RegistroComponent]
 })
 export class AuthModule { }
