abbdb1d662f1eb479b78d361828d2b88b76d4105
Santiago Salazar <s.salazara@uniandes.edu.co>
2018-11-26 23:04:10 -0500

    Cambios peques en estilos de ingresar y registrarse.

--- src/app/comunes/navbar/navbar.component.ts
+++ src/app/comunes/navbar/navbar.component.ts
@@ -8,12 +8,16 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
+  usuario: any;
+
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.usuario = this.authService.getUser();
   }
 
   logout(): void {
+    this.usuario = undefined;
     this.authService.logout()
   }
 }
